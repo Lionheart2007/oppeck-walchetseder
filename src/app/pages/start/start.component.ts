fb17157082e913d7dfec8ed3827ae1290e5ab021
Leonhard Holmer Patoschka <e12120510@student.tuwien.ac.at>
2024-12-02 22:05:09 +0100

    Feat: Scafold start page

---
+++ src/app/pages/start/start.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-start',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './start.component.html',
+  styleUrl: './start.component.css',
+})
+export class StartComponent {}
