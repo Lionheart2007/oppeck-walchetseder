1b414982c4fd94cf8a8dbee2775a01f3206f7acb
Leonhard Holmer Patoschka <e12120510@student.tuwien.ac.at>
2024-12-02 21:37:04 +0100

    Feat: Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2c4ca2",
+        },
+        muted: {
+          DEFAULT: "#9ca3af",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
