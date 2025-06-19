9c4241558e85c24fa2269f267fd7e019af6e2967
Leonhard Patoschka <leonhard.patoschka@nuntiate.com>
2025-06-19 19:41:33 +0200

    used generalInfo component on start page

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GeneralInfoComponent } from "../../common/general-info/general-info.component";
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
+  imports: [GeneralInfoComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.css',
 })
